f8a4dcf9cd8a67ef211728f2fc1c16939651cab6
vitorreiel <vitorreiel@hotmail.com>
2021-10-28 18:49:29 -0300

    Atividade 04 Resolvida.

--- atividades/atividade04/saudacao.sh
+++ atividades/atividade04/saudacao.sh
@@ -1,7 +1,6 @@
+#!/bin/bash
 d=$(date +%d)
 m=$(date +%m)
 a=$(date +%G)
 u=$USER
-n=$(echo 'Olá' $u',\nHoje é dia '$d', do mês '$m' do ano de '$a'.')
-echo -e "$n"
-echo -e "$n" >> saudacao.log
+echo -e 'Olá' $u',\nHoje é dia '$d', do mês '$m' do ano de '$a'.' | tee -a saudacao.log
