afac9852e026d09fe20f7c160bb68b1ad8379a99
vitorreiel <vitorreiel@hotmail.com>
2021-11-11 20:56:56 -0300

    Atividade 06 Resolvida.

---
+++ atividades/atividade06/cinco_diretorios.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+mkdir cinco
+cd cinco
+for i in {1..5}; do
+	mkdir dir$[i]
+	cd dir$[i]
+	for l in {1..4}; do
+		case $l in 1)
+			echo -e "1" > arq$[l].txt;;
+		2)
+			echo -e "2\n2" > arq$[l].txt;;
+		3)
+			echo -e "3\n3\n3" > arq$[l].txt;;
+		4)
+			echo -e "4\n4\n4\n4" > arq$[l].txt
+			cd ..;;
+		esac
+	done
+done
+echo "Diretórios criados com sucesso!"
---
+++ atividades/atividade06/latencia.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "Aguarde um momento... Isso pode demorar um pouquinho."
+for i in {1..5}; do
+	array[i]=$(sed -n $[i]p enderecos_ip.txt)
+	ping -c 10 ${array[i]} | sed -n '/^rtt/p' | cut -f5 -d'/' >> teste.txt
+	echo $( echo ${array[i]} & sed -n $[i]p teste.txt )"ms" >> teste2.txt
+	if [ $i == 3 ]; then
+		echo "Quase lá... Calma Calma."
+	fi
+done
+sort teste2.txt
+rm teste.txt teste2.txt
