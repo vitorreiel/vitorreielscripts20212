20f932b4f98145ebb4e595c0f5c19deac437d084
vitorreiel <52376553+vitorreiel@users.noreply.github.com>
2022-01-11 14:25:33 -0300

    Atividade 12 Resolvida.

--- atividades/atividade12/servidorWeb.sh
+++ atividades/atividade12/servidorWeb.sh
@@ -8,7 +8,7 @@ PORT80=$(aws ec2 authorize-security-group-ingress --group-id $GRUPO --protocol t
 
 INSTANCIA=$(aws ec2 run-instances --image-id "ami-083602cee93914c0c" --instance-type t2.micro --key-name $1 --region us-east-1 --security-group-ids $GRUPO --subnet-id $SUBREDE --user-data file://instalacaoSV.sh --query "Instances[0].InstanceId" --output text)
 
-echo "Criando servidor... Por favor, Aguarde."
+echo "Criando servidor... Por favor, Aguarde. "
 sleep 50
 IP=$(aws ec2 describe-instances --instance-id $INSTANCIA --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
 echo "Acesse: http://"$IP"/"
