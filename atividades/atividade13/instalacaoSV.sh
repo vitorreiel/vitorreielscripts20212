63cf8285d341fafc9e98553e65f9559ab9c32242
vitorreiel <vitorreiel@hotmail.com>
2022-01-22 00:34:15 -0300

    Atividade 13 Resolvida.

---
+++ atividades/atividade13/instalacaoSV.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+sudo yum install -y httpd
+sudo systemctl start httpd
+sudo systemctl enable hhtpd
+sudo touch /var/www/html/texto.html
+sudo chown ec2-user /var/www/html/texto.html
+
+echo "[Unit]
+Description=Atividade13
+After=network.target
+StartLimitIntervalSec=0
+[Service]
+Type=simple
+Restart=always
+RestartSec=1
+User=ec2-user
+ExecStart=/home/ec2-user/monitoramento.sh
+
+[Install]
+WantedBy=multi-user.target" > /etc/systemd/system/serviceVRML.service
+
+echo "<html>
+        <head>
+        <title>Atividade 13</title>
+                <style>
+                        #i{
+                                width: 25%;
+                                height: 80%;
+                                border: 3px solid #ccc;
+                                border-radius: 20px;
+                                margin-top: 5%;
+                                margin-left: 36%;
+                                background-color: white;
+                                padding-left: 40px;
+                        }
+                        body{
+                                background-image: linear-gradient(to bottom, white, #CFFFE5, #C2E5D3);
+                        }
+                </style>
+        </head>
+        <body>
+                <div>
+                        <iframe src='texto.html' id='i'></iframe>
+                </div>
+        </body>
+</html>" > /var/www/html/index.html
+cat <<EOF > /home/ec2-user/monitoramento.sh
+#!/bin/bash
+while true;
+do
+        DATA=\$(date +%H:%M:%S-%D)
+        TEMP=\$(uptime | sed 's/:[0-9][0-9] /\n/g;s/  /\n/g' | head -2 | tail -1)
+        CMS=\$(uptime | sed 's/  /\n/g' | head -4 | tail -1)
+        MEML=\$(free | head -2 | tail -1 | egrep -o '\w+' | head -4 | tail -1)
+        MEMU=\$(free | head -2 | tail -1 | egrep -o '\w+' | head -3 | tail -1)
+        BYT=\$(cat /proc/net/dev | tail -1 | egrep -o '\w+' | head -2 | tail -1)
+        TRN=\$(cat /proc/net/dev | tail -1 | egrep -o '\w+' | head -10 | tail -1)
+        echo -e '<h2>'\$DATA '<br>\n'\$TEMP '<br>\n'\$CMS '<br>\nMemória livre: '\$MEML ', ocupada: '\$MEMU '<br>\nBytes recebidos: '\$BYT ', enviados: '\$TRN '<br>\n<hr width="50%" color="#ccc"></hr>\n' > /var/www/html/texto.html
+sleep 5
+done
+EOF
+sudo chmod +x /home/ec2-user/monitoramento.sh
+sudo chmod +x /etc/systemd/system/serviceVRML.service
+sudo systemctl start serviceVRML.service
+sudo systemctl enable serviceVRML.service
--- atividades/atividade13/instanciaServico.sh
+++ atividades/atividade13/instanciaServico.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
-# Correção: 4,0. Tudo OK!!!
-
 SUBREDE=$(aws ec2 describe-subnets --query 'Subnets[0].SubnetId' --output text)
-GRUPO=$(aws ec2 create-security-group --group-name "scriptsVRM" --description "Grupo de Seguranca para Scripts" --output text)
+GRUPO=$(aws ec2 create-security-group --group-name "scriptsVRML" --description "Grupo de Seguranca para Scripts" --output text)
 
 PORT22=$(aws ec2 authorize-security-group-ingress --group-id $GRUPO --protocol tcp --port 22 --cidr 0.0.0.0/0)
 PORT80=$(aws ec2 authorize-security-group-ingress --group-id $GRUPO --protocol tcp --port 80 --cidr 0.0.0.0/0)
 
 INSTANCIA=$(aws ec2 run-instances --image-id "ami-083602cee93914c0c" --instance-type t2.micro --key-name $1 --region us-east-1 --security-group-ids $GRUPO --subnet-id $SUBREDE --user-data file://instalacaoSV.sh --query "Instances[0].InstanceId" --output text)
 
-sudo yum install -y httpd
-sudo systemctl start httpd
-echo "<html><h1>Vitor Reiel Moura de Lima<p>499077</p></h1></html>" > /var/www/html/index.html
-
-echo "Criando servidor... Por favor, Aguarde."
+echo "Criando servidor de Monitoramento... Por favor, Aguarde."
 sleep 50
 IP=$(aws ec2 describe-instances --instance-id $INSTANCIA --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
 echo "Acesse: http://"$IP"/"
