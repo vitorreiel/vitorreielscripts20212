fad64824818805556b0f745dbf9815d2a7383102
vitorreiel <vitorreiel@hotmail.com>
2022-02-04 13:07:51 -0300

    Atividade 15

--- atividades/atividade15/cliente.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo yum install -y mariadb mariadb-server
-sudo systemctl start mariadb
-sudo systemctl enable mariadb
-echo -e "[client]\nuser="vitor"\npassword="limaa > /home/ec2-user/.my.cnf
-echo -e "(mysql -u '"vitor"' scripts -h '"172.31.15.144"')<<EOF\nUSE scripts;\nCREATE TABLE Teste ( atividade INT );\nEOF\nrm /home/ec2-user/scripts.sh" > /home/ec2-user/scripts.sh
-sudo chmod +x /home/ec2-user/scripts.sh
-cd /home/ec2-user
-./scripts.sh
--- atividades/atividade15/servidor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo yum install -y mariadb-server
-sudo systemctl start mariadb
-sudo systemctl enable mariadb
-echo -e "mysql<<EOF\nCREATE DATABASE scripts;\nCREATE USER '"vitor"'@'%' IDENTIFIED BY '"limaa"';\nGRANT ALL PRIVILEGES ON scripts.* TO '"vitor"'@'%';\nUSE scripts;\nEOF\nrm /home/ec2-user/scripts.sh" > /home/ec2-user/scripts.sh
-sudo chmod +x /home/ec2-user/scripts.sh
-cd /home/ec2-user
-./scripts.sh
